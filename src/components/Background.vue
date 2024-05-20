ff4fdfd44d0cf64540ceb2ed0dcfee8a2fb9c97e
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-05-20 22:25:31 +0800

    Bing isMobile

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -41,7 +41,8 @@ const changeBg = (type) => {
   if (type == 0) {
     bgUrl.value = `/images/background${bgRandom}.jpg`;
   } else if (type == 1) {
-    bgUrl.value = "https://bingw.jasonzeng.dev/";
+    const isMobile = window.innerWidth < 768;
+    bgUrl.value = `https://bingw.jasonzeng.dev/?resolution=${isMobile ? "768x1280" : "1920x1080"}&index=random`;
   } else if (type == 2) {
     bgUrl.value = "https://api.aixiaowai.cn/gqapi/gqapi.php";
   } else if (type == 3) {

d1e8c25a5869cf542d5e29ad1556604bc30dc874
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-05-20 15:32:47 +0800

    翻译Wallpaper设置项

--- src/components/Set.vue
+++ src/components/Set.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="setting">
     <el-collapse class="collapse" v-model="activeName" accordion>
-      <el-collapse-item title="个性壁纸" name="1">
+      <el-collapse-item title="Custom Wallpaper" name="1">
         <div class="bg-set">
           <el-radio-group v-model="coverType" text-color="#ffffff" @change="radioChange">
-            <el-radio value="0" size="large" border>默认壁纸</el-radio>
-            <el-radio value="1" size="large" border>每日一图</el-radio>
+            <el-radio value="0" size="large" border>Default</el-radio>
+            <el-radio value="1" size="large" border>Bing Wallpaper</el-radio>
+            <!--
             <el-radio value="2" size="large" border>随机风景</el-radio>
             <el-radio value="3" size="large" border>随机动漫</el-radio>
+            -->
           </el-radio-group>
         </div>
       </el-collapse-item>
